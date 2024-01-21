90be6393847f840d1251fab2d472399f12954251
Tanvir Ahmed Khan <mostlytanvir@gmail.com>
2024-01-21 10:54:56 +0600

    Updated User Mode with unique fields,

--- src/main/java/com/example/VideoShareApp/model/User.java
+++ src/main/java/com/example/VideoShareApp/model/User.java
@@ -2,11 +2,11 @@ package com.example.VideoShareApp.model;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.annotation.Reference;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
-import java.util.UUID;
 
 @Getter
 @Setter
@@ -17,11 +17,15 @@ import java.util.UUID;
 public class User {
     @Id
     private String userId;
+
+    @Indexed(unique = true)
     private String name;
+
+    @Indexed(unique = true)
     private String email;
+
     private String password;
 
     @Reference
     private List<Video> uploadedVideos;
-
 }
